93a09d05236b2c96a981cb5c34179053d5c24553
雪鈴 SnowLin <113241163+snowlinouo@users.noreply.github.com>
2024-11-04 13:42:26 +0800

    Update display-setting.vue

--- components/Console/display-setting.vue
+++ components/Console/display-setting.vue
@@ -3,8 +3,7 @@ import {useDisplaySettingStore} from "~/store/displaySettingStore";
 import {useFontStore} from "~/store/fontStore";
 import {useAnimateSettingStore} from "~/store/animateSettingStore";
 import { NColorPicker, NConfigProvider, darkTheme } from 'naive-ui'
-import { dateZhTW, zhTW } from 'naive-ui'
-import type { NDateLocale, NLocale } from 'naive-ui'
+import { zhTW, dateZhTW } from 'naive-ui'
 
 const displaySettingStore = useDisplaySettingStore();
 const {
@@ -27,61 +26,66 @@ const fontStore = useFontStore();
 </script>
 
 <template>
-  <NConfigProvider :theme="darkTheme" :locale="zhTW" :date-locale="zhTW">
+  <NConfigProvider :theme="darkTheme" :locale="zhTW" :dateLocale="dateZhTW">
     <div id="Display-Setting">
       <Fieldset legend="時間字型設定" class="w-full">
-        <div class="flex flex-col justify-between gap-y-3">
-          <InputGroup class="clock-font">
-            <InputGroupAddon>
-              時間字型
-            </InputGroupAddon>
-            <Select v-model="clockFont"
-                    :options="fontStore.fontOptions"
-                    optionLabel="name"
-                    optionValue="value"
-                    placeholder="時間字型"
-                    checkmark
-                    :highlightOnSelect="false"
-                    class="w-full md:w-56"
-            />
-          </InputGroup>
-          <InputGroup class="clock-font">
-            <InputGroupAddon>
-              字型大小
-            </InputGroupAddon>
-            <InputNumber
-                v-model="clockFontSize"
-                inputId="minmax-buttons"
-                mode="decimal"
-                showButtons
-                :min="12"
-                :max="144"
-                fluid/>
-            <InputGroupAddon>
-              px
-            </InputGroupAddon>
-          </InputGroup>
-          <div>
+        <Fluid>
+        <div class="grid grid-cols-2 gap-3">
+            <div class="col-span-full">
+              <InputGroup class="clock-font">
+              <InputGroupAddon>
+                時間字型
+              </InputGroupAddon>
+                <Select v-model="clockFont"
+                        :options="fontStore.fontOptions"
+                        optionLabel="name"
+                        optionValue="value"
+                        placeholder="時間字型"
+                        checkmark
+                        :highlightOnSelect="false"
+                        class="w-full md:w-56"/>
+              </InputGroup>
+            </div>
+            <div class="col-span-full">
+              <InputGroup class="clock-font">
+                <InputGroupAddon>
+                  字型大小
+                </InputGroupAddon>
+                <InputNumber
+                    v-model="clockFontSize"
+                    inputId="minmax-buttons"
+                    mode="decimal"
+                    showButtons
+                    :min="12"
+                    :max="144"
+                    fluid/>
+                <InputGroupAddon>
+                  px
+                </InputGroupAddon>
+              </InputGroup>
+            </div>
+            <div>
               <label for="non-fluid" class="font-bold mb-2 block">邊框顏色</label>
               <n-color-picker v-model:value="clockBorderColor" :modes="['hex']" :actions="['clear']" size="large" placement="bottom" :show-alpha="false"/>
+            </div>
+            <div>
+              <label for="non-fluid" class="font-bold mb-2 block">邊框粗細</label>
+              <InputGroup class="clock-font">
+                <InputNumber
+                    v-model="clockBorderSize"
+                    inputId="minmax-buttons"
+                    mode="decimal"
+                    showButtons
+                    :min="0"
+                    :max="Math.floor(clockFontSize / 10)"
+                    fluid/>
+                <InputGroupAddon>
+                  px
+                </InputGroupAddon>
+              </InputGroup>
+            </div>
           </div>
-          <InputGroup class="clock-font">
-            <InputGroupAddon>
-              邊框粗細
-            </InputGroupAddon>
-            <InputNumber
-                v-model="clockBorderSize"
-                inputId="minmax-buttons"
-                mode="decimal"
-                showButtons
-                :min="0"
-                :max="Math.floor(clockFontSize / 10)"
-                fluid/>
-            <InputGroupAddon>
-              px
-            </InputGroupAddon>
-          </InputGroup>
-        </div>
+        </Fluid>
       </Fieldset>
 
       <Fieldset legend="動畫設定" class="w-full">
